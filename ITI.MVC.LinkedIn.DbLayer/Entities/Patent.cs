efc46359a20e06b442e2803ba5c53061dafba75f
hala samir <33582359+hala-gaafar@users.noreply.github.com>
2020-01-31 19:40:02 +0200

    create Hala's Entity

---
+++ ITI.MVC.LinkedIn.DbLayer/Entities/Patent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.MVC.LinkedIn.DbLayer.Entities
+{
+    public class Patent
+    {
+        [Key]
+        [Column(Order = 0)]
+        public String Title { get; set; }
+
+        [Required]
+        public List<Organization> Country { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        public String Inventor { get; set; }
+
+        [Required]
+        public int Status { get; set; }
+
+
+        public DateTime Date { get; set; }
+
+
+        public String Url { get; set; }
+
+
+        public String Description { get; set; }
+
+        [Key]
+        [Column(Order =1)]
+        [ForeignKey("UserID")]
+        public int UserId { get; set; }
+
+
+        public virtual User UserID { get; set; }
+
+    }
+}
