1a5f20443b3ccc461b1fd688a0a7a7e9ce3e8b70
hala samir <33582359+hala-gaafar@users.noreply.github.com>
2020-01-31 19:40:02 +0200

    create Hala's Entity

---
+++ ITI.MVC.LinkedIn.DbLayer/Entities/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.MVC.LinkedIn.DbLayer.Entities
+{
+    public class Course
+    {
+        [Key]
+        [Column(Order = 0)]
+        public String Name { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        public List<Organization> WorkExperienceId { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("UserID")]
+        public int UserId { get; set; }
+
+
+        public virtual User UserID { get; set; }
+    }
+}
