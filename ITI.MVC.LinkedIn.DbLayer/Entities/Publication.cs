4b3c3eff63614eb6f3069ff0f26ded2418624282
khaled <khaledyousef434@gmail.com>
2020-02-01 09:41:20 +0200

    upload classes

---
+++ ITI.MVC.LinkedIn.DbLayer/Entities/Publication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.MVC.LinkedIn.DbLayer.Entities
+{
+   public class Publication
+    {
+        [Key, Column(Order = 0)]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="please enter Title ")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {1} characters long.", MinimumLength = 6)]
+
+        public int Title { get; set; }
+        public string Publisher { get; set; }
+        public DataType Date { get; set; }
+        public string Author { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+       
+        
+        [Key, Column(Order = 1)]
+        public int UserId { get; set; }
+               
+    }
+}
