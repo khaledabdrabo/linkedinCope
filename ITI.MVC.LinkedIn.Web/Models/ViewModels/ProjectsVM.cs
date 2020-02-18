63794600c407c2aa1840d4667a35e1269cd0ff95
Mohamed Ali <Mohamed.vip9522@gmail.com>
2020-02-18 19:39:24 +0200

    Add More Modals to Profile Page and Changes

---
+++ ITI.MVC.LinkedIn.Web/Models/ViewModels/ProjectsVM.cs
@@ -0,0 +1,16 @@
+﻿using ITI.MVC.LinkedIn.Web.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITI.MVC.LinkedIn.Web.Models.ViewModels
+{
+    public class ProjectsVM
+    {
+        public MonthEnum StartMonth { get; set; }
+        public int StartYear { get; set; }
+        public MonthEnum EndMonth { get; set; }
+        public int EndYear { get; set; }
+    }
+}
